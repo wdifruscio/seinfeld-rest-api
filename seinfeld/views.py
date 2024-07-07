aefc373f1edd3bafb662a40ff476278feb9df622
wdifruscio <wdifruscio@gmail.com>
2024-07-06 21:19:57 -0300

    chore(cache): added infinite cache to viewsets

--- seinfeld/views.py
+++ seinfeld/views.py
@@ -1,5 +1,7 @@
 from django.db.models import Count, Max
 from django.db.models.functions import Length
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.decorators import action
 from rest_framework.pagination import PageNumberPagination
@@ -24,6 +26,14 @@ class QuoteViewSet(ReadOnlyModelViewSet):
     filterset_class = SentenceFilter
     pagination_class = Pagination
 
+    @method_decorator(cache_page(None))
+    def retrieve(self, request, *args, **kwargs):
+        return super().retrieve(request, *args, **kwargs)
+
+    @method_decorator(cache_page(None))
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
     @action(detail=False, methods=["GET"])
     def random(self, request):
         return Response(
@@ -38,6 +48,14 @@ class UtteranceViewSet(ReadOnlyModelViewSet):
     filter_backends = (DjangoFilterBackend,)
     filterset_class = SentenceFilter
     pagination_class = Pagination
+
+    @method_decorator(cache_page(None))
+    def retrieve(self, request, *args, **kwargs):
+        return super().retrieve(request, *args, **kwargs)
+
+    @method_decorator(cache_page(None))
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
 
 
 
