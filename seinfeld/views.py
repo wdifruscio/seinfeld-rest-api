479dc676e0a203a909de9b1269ee9f9cd2be71dc
wross <wdifruscio@gmail.com>
2024-07-11 12:05:47 -0600

    chore(ratelimit): added limit to api endpoints

--- seinfeld/views.py
+++ seinfeld/views.py
@@ -1,6 +1,7 @@
 from django.utils.decorators import method_decorator
 from django.views.decorators.cache import cache_page
 from django_filters.rest_framework import DjangoFilterBackend
+from django_ratelimit.decorators import ratelimit
 from rest_framework.decorators import action
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
@@ -30,20 +31,22 @@ class QuoteViewSet(ReadOnlyModelViewSet):
     filterset_class = SentenceFilter
     pagination_class = Pagination
 
+    @method_decorator(ratelimit(key='ip', rate='30/h', method='GET', block=True))
     @method_decorator(cache_page(None))
     def retrieve(self, request, *args, **kwargs):
         return super().retrieve(request, *args, **kwargs)
 
+    @method_decorator(ratelimit(key='ip', rate='30/h', method='GET', block=True))
     @method_decorator(cache_page(None))
     def list(self, request, *args, **kwargs):
         return super().list(request, *args, **kwargs)
 
+    @method_decorator(ratelimit(key='ip', rate='30/h', method='GET', block=True))
     @action(detail=False, methods=["GET"])
     def random(self, request):
         return Response(
             self.get_serializer(get_random_from_queryset(self.get_queryset())).data,
         )
-
 
 
 class UtteranceViewSet(ReadOnlyModelViewSet):
@@ -54,10 +57,14 @@ class UtteranceViewSet(ReadOnlyModelViewSet):
     filterset_class = SentenceFilter
     pagination_class = Pagination
 
+    @method_decorator(ratelimit(key='ip', rate='30/h', method='GET', block=True))
     @method_decorator(cache_page(None))
+    @ratelimit(key='ip', rate='50/hr')
     def retrieve(self, request, *args, **kwargs):
         return super().retrieve(request, *args, **kwargs)
 
+    @method_decorator(ratelimit(key='ip', rate='30/h', method='GET', block=True))
     @method_decorator(cache_page(None))
+    @ratelimit(key='ip', rate='50/hr')
     def list(self, request, *args, **kwargs):
         return super().list(request, *args, **kwargs)
--- seinfeld_api/settings.py
+++ seinfeld_api/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'django_ratelimit.middleware.RatelimitMiddleware',
 ]
 
 REST_FRAMEWORK = {
